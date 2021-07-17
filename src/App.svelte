eb799b0f1b824ff4abeff1f8666be5404906e182
TitovMaximDPG <titovmaximdpg@gmail.com>
2021-07-17 12:59:04 +0500

    1

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class="block">
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v11.0&appId=3253341138222259&autoLogAppEvents=1" nonce="mN1gecH0"></script>
+
+    <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+</div>
+
+<style> </style>
